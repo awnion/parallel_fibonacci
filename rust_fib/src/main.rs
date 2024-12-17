1def35e557f8d29f34c651fbe4b6ecd19f43c081
Sergei Blinov <blinovsv@gmail.com>
2024-12-17 16:41:23 +0100

    rust: bump deps

--- rust_fib/src/main.rs
+++ rust_fib/src/main.rs
@@ -1,6 +1,5 @@
 #[tokio::main]
 async fn main() {
     const N: u64 = 52;
-    // println!("fib {N} = {}", fib2(N).await);
-    println!("fib {N} = {}", rust_fib::fib4(N));
+    println!("fib {N} = {}", rust_fib::fib(N).await);
 }
